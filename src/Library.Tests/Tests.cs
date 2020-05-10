9a6943bbb552e07f4f04fdd96dcf6e9d094e8e7b
Konstantin S <havendv@gmail.com>
2020-05-11 04:12:56 +1000

    Updated tests

--- src/Library.Tests/Tests.cs
+++ src/Library.Tests/Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,13 +14,17 @@ namespace ClearComApi.Tests
         {
             using var source = new CancellationTokenSource(TimeSpan.FromMinutes(1));
 
-            using var client = ApiClient.New("http://clearcomtest.net/api/", "admin", "4Mercury@62");
+            using var client = ApiClient.New("http://clearcomtest.net/api/1/", "admin", "4Mercury@62");
 
             var capabilities = await client.GetDevicesCapabilitiesAsync(source.Token);
 
             foreach (var capability in capabilities)
             {
-                Console.WriteLine(capability);
+                Console.WriteLine($"Capability. {capability?.Type}");
+                foreach (var @event in capability?.Events ?? new List<Events>())
+                {
+                    Console.WriteLine($"Event. Type: {@event?.Type}. Value: {@event?.Value}");
+                }
             }
         }
     }
